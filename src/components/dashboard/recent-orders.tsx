1fcad9eef84f05fbd40f120b153b2e3d58701303
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-08-31 00:11:56 +0600

    add

--- src/components/dashboard/recent-orders.tsx
+++ src/components/dashboard/recent-orders.tsx
@@ -15,7 +15,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table";
+} from "@/src/components/ui/table";
 import { Order } from "@/src/types";
 
 // Mock data for the table
